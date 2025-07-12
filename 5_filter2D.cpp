3d520a3658a64662dd6d9baaf23ebf42d04c9d85
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2025-07-12 21:08:32 +0200

    convolution

---
+++ 5_filter2D.cpp
@@ -0,0 +1,58 @@
+#include <opencv2/opencv.hpp>
+
+int main(){
+    // cv::Mat_<float> image(5, 5);
+    // image << 255, 255, 255, 000, 000,
+    //          255, 255, 255, 000, 000,
+    //          255, 255, 255, 000, 000,
+    //          000, 000, 000, 000, 000,
+    //          000, 000, 000, 000, 000;
+
+    cv::Mat image = cv::imread("resourses/apple.jpeg");
+    cv::Mat im8, fil2D, filter2D8;
+
+    cv::Mat_<float> kernel(3,3);
+    // detecting edges
+    // kernel << -1,  1, -1,
+    //            1,  0,  1,
+    //           -1,  1, -1;
+
+    // enhance sharpness
+    // kernel << -1,  1, -1,
+    //            1,  1,  1,
+    //           -1,  1, -1;
+    
+    // blur
+    // kernel << 0.11,  0.11, 0.11,
+    //            0.11,  0.11,  0.11,
+    //           0.11,  0.11, 0.11;
+
+    // gauss blur
+    // kernel << 0.0625,  0.125, 0.0625,
+    //            0.125,  0.25,  0.125,
+    //           0.0625,  0.125, 0.0625;
+
+    // emboss
+    // kernel << -1,  -1,  0,
+    //           -1,   0,  1,
+    //            0,   1,  1;
+
+    // motion blur
+    kernel << 0.33,    0,    0, 
+              0,    0.33,    0,
+              0,    0,    0.33;
+
+    cv::namedWindow("image", cv::WINDOW_NORMAL);
+    cv::namedWindow("filter2D", cv::WINDOW_NORMAL);
+
+    cv::filter2D(image, fil2D, -1, kernel);
+
+    image.convertTo(im8, CV_8U);
+    fil2D.convertTo(filter2D8, CV_8U);
+
+    cv::imshow("image", im8);
+    cv::imshow("filter2D", filter2D8);
+
+    cv::waitKey(0);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Using OpenCV with C++
+
+## Prerequisites
+1) OpenCV library should be installed.
+2) Add to Path environment variable path to ....\OpenCV-MinGW-Build\include and ....\OpenCV-MinGW-Build\x64\mingw\bin
+3) Compilation should be done with explicit including OpenCV libraries. it is example of command:
+```
+g++ -o 2_filtering.exe 2_filtering.cpp -IC:\DY\Programs\OpenCV-MinGW-Build\include -IC:\DY\Programs\OpenCV-MinGW-Build\include\opencv4 -LC:\DY\Programs\OpenCV-MinGW-Build\x64\mingw\lib -lopencv_core455 -lopencv_imgcodecs455 -lopencv_highgui455 -lopencv_imgproc455
+```
+to simplify compilation routine I described those libraries in tasks.json file so VScode use it during build process. 
+
+## Source code files description
 * 1_imshow.cpp - open and display image
 * 2_filtering.cpp - comparation of bilateral filter, Gauss blur and median blur.
 * 3_erosion-dilation.cpp - erosion and delation of image with different kernel size.
+* 4_sobel.cpp - Sobel operator to detect the edges. You can investigate the influence of the arguments (dx, dy, kernel, scale and delta) on the result.
+* 5_filted2D - convolution with custom kernels. Blur, edge detection, sharpness enhancement, motion blur, e.t.c.
